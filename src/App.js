3a42f76bbbd1260bd56d74ebba0584ef876f8f9b
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-15 15:02:13 -0300

    Adding form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import ServicosCards from "./components/Servicos";
+import Form from "./components/Form";
+
 
 function App() {
   return (
     <>
       <NavBar />
       <ServicosCards />
+      <Form />
       <Footer />
     </>
   );
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,9 +1,25 @@
 import "./index.scss";
 
-const form = () => {
+const Form = () => {
   return (
     <>
-      <form></form>
+      <div className="form-contact">
+        
+      <form>
+      <h2>Fale conosco</h2>
+        <div className="form-nomes">
+          <input type="text" placeholder="Nome" />
+          <input type="text" placeholder="Sobrenome" />
+        </div>
+        <div className="form-informacoes" >
+          <input type="tel" placeholder="Contato" className="telefone"/>
+          <input type="text" placeholder="Descrição" className="descricao" />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+      </div>
     </>
   );
 };
+
+export default Form
