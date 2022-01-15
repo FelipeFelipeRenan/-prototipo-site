ae8bc227a4050a6149d1affec51d5d877aa10658
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-15 18:01:17 -0300

    Cleaning some stuff

--- src/components/Clientes/index.jsx
+++ src/components/Clientes/index.jsx
@@ -12,8 +12,9 @@ const Cliente = () =>{
                     <img src="logo-ufca.jpg" alt="Logo UFCA" />
                 </div>
                 <div className="sobre">
-                    <h1>Sobre nós</h1>
+                    
                     <div className="sobre-text">
+                    <h1>Sobre nós</h1>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad voluptatum quasi labore placeat reiciendis, aut nisi, aliquid ab officiis vero quam dolorem eos corporis optio cupiditate ratione accusamus dolores?
                     </div>
                 </div>
