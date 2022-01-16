51e614260f2e62adc6eed72dc3130eb4bfd3a78e
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-16 15:23:20 -0300

    Adding crew members area and testing some carrousels

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import NavBar from "./components/NavBar";
 import ServicosCards from "./components/Servicos";
 import Form from "./components/Form";
 import Cliente from "./components/Clientes";
+import Equipe from "./components/Equipe-layout";
 
 
 function App() {
   return (
     <>
       <NavBar />
+      <Equipe />
       <Cliente />
       <ServicosCards />
       <Form />
